d11f4ba0a12f11eaae8e3f5b6ce7cd2594be616f
luiz0217 <luiz.padletski@escolaoptimus.com>
2025-05-26 20:25:23 -0300

    teste aulas

--- app/Http/Controllers/AulasController.php
+++ app/Http/Controllers/AulasController.php
@@ -61,7 +61,7 @@ class AulasController extends Controller
     
     $aula = aulas::where('user_id',$user['id'])->where('id',$request['aula_id'])->first();
     
-    return response()->json($aula->turma->alunos);
+    //return response()->json($aula->turma->alunos);
     $aula->turma;
     $aula->turma->alunos;
     $aula->treino;
@@ -75,7 +75,7 @@ class AulasController extends Controller
     
     */
     
-    //return response()->json($aula);
+    return response()->json($aula);
     
 
         
