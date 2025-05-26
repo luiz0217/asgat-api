7a4c33ec10d7fdcf9e33531632f8f726454c21dc
luiz0217 <luiz.padletski@escolaoptimus.com>
2025-05-26 17:35:32 -0300

    ajustes nas aulas

--- app/Http/Controllers/AulasController.php
+++ app/Http/Controllers/AulasController.php
@@ -66,7 +66,7 @@ class AulasController extends Controller
     
             return response()->json($aula);
         } catch (\Throwable $th) {
-            throw $th;
+           return response()->json(['erro'=> $th]);
         }
     }
 
